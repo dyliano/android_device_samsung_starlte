388fc4bebc27856c4ea7cac1935a4e800a56fd21
fuxk u <dylan.is.008@gmail.com>
2019-05-12 11:46:31 +0100

    starlte: setting the desired OpenGapps variant

--- device.mk
+++ device.mk
@@ -2,9 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+GAPPS_VARIANT := mini
+GAPPS_EXCLUDED_PACKAGES := Hangouts
+GAPPS_PRODUCT_PACKAGES += Chrome GoogleContacts PrebuiltBugle
 
 # Property overrides
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Inherit common device configuration
 $(call inherit-product, device/samsung/star-common/star-common.mk)
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
